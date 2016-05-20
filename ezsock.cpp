e95d5b1a16252eaeee9d67e64da3559d69c42b22
Siriwat Kasamwattanarote <siriwat@live.jp>
2016-05-20 16:56:53 +0900

    Ready for making a connection for both server and client.

--- ezsock.cpp
+++ ezsock.cpp
@@ -6,28 +6,129 @@
 ** Author: Siriwat Kasamwattanarote
 ** -------------------------------------------------------------------------*/
 
-#include <cstdlib>
-#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>			// memset,memcpy
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>			// gethostbyname
+#include <cstdlib>
+#include <iostream>
+#include <string>
 
 #include "ezsock.h"
 
 using namespace std;
 
-ezsock::ezsock(sock_role role)
+ezsock::ezsock(sock_role role, int port, const string& ip)
 {
-	if (role == sock_role::server)
-		exit(0);
+	// Socket initialization
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+		error("ERROR opening socket");
+
+	// Get general parameters
+	portno = port;
+	_role = role;
+
+	switch (_role) {
+	case sock_role::Server: {
+		// Set server params
+		memset((char *) &serv_addr, 0, sizeof(serv_addr));
+		serv_addr.sin_family = AF_INET;
+		serv_addr.sin_addr.s_addr = INADDR_ANY;
+		serv_addr.sin_port = htons(portno);		
+		clilen = sizeof(cli_addr);
+		
+		// Binding
+		if (bind(sockfd, (struct sockaddr *) &serv_addr,
+		         sizeof(serv_addr)) < 0)
+			error("ERROR on binding");
+		
+		// Listening for a connection
+		listen(sockfd, queue);
+		
+		// Accepting connection
+		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+		if (newsockfd < 0)
+			error("ERROR on accept");
+
+		break;
+	}
+	case sock_role::Client: {
+		// Get target server
+		server = gethostbyname(ip.c_str());
+
+		if (server == NULL) {
+			fprintf(stderr,"ERROR, no such host\n");
+			exit(0);
+		}
+		
+		// Set server params
+		memset((char *) &serv_addr, 0, sizeof(serv_addr));
+		serv_addr.sin_family = AF_INET;
+		memcpy(	(char *)server->h_addr,
+		        (char *)&serv_addr.sin_addr.s_addr,
+		        server->h_length);
+		serv_addr.sin_port = htons(portno);
+		
+		// Request for connection
+		if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+			error("ERROR connecting");
+
+		break;
+	}}
 }
 
 ezsock::~ezsock(void)
 {
+	release();
+}
+
+string ezsock::read_txt()
+{		
+	memset(buffer, 0, 256);
+	n = read(newsockfd, buffer, 255);
+	cout << "n: " << n << endl;
+	if (n < 0) error("ERROR reading from socket");
+	//cout << "Here is the message: " << string(buffer) << "[" << strlen(buffer) << "]" << endl;
+	n = write(newsockfd, "I got your message", 18);
+	cout << "n: " << n << endl;
 	
 }
-	
+
+void ezsock::send_txt(const string &text)
+{
+	printf("Please enter the message: ");
+	memset(buffer, 0, 256);
+	cin >> buffer;
+	int cnt = 10;
+	while (cnt--) {
+		
+		n = write(sockfd,buffer,256);
+		cout << "n: " << n << endl;
+		if (n < 0)
+			error("ERROR writing to socket");
+		memset(buffer, 0, 256);
+		n = read(sockfd,buffer, 255);
+		cout << "n: " << n << endl;
+		if (n < 0)
+			error("ERROR reading from socket");
+		printf("%s\n",buffer);
+		
+	}
+}
+
+void ezsock::error(const string &msg)
+{
+	error(msg);
+	exit(0);
+}
+
 void ezsock::release()
 {
-	
+	if (_role == sock_role::Server)
+		close(newsockfd);
+	close(sockfd);
 }
 //;)
--- ezsock.h
+++ ezsock.h
@@ -6,28 +6,47 @@
 ** Author: Siriwat Kasamwattanarote
 ** -------------------------------------------------------------------------*/
 
+/* How a socket work?
+ * 
+ * 1. A socket is created with socket().
+ * 2. The socket is bound to a local address using bind(),
+ *    so that other sockets may be connect()ed to it.
+ * 3. A willingness to accept incoming connections and a queue limit for incoming connections are specified with listen().
+ * 4. Connections are accepted with accept().
+ * 5. Any transmission can be done between server and client using read() and write() on the socket file descriptor (socksd)
+ * 6. Terminating the connection by using close(socket)
+ * */
+
 
 using namespace std;
 
 
 class ezsock
-{
+{	
+	enum sock_role { Server, Client };
 	int sockfd, newsockfd, portno;
+	char *buffer;
+	sock_role _role;
+	
+	// Server
+	struct sockaddr_in cli_addr;
 	unsigned int clilen;
-	char* buffer;
-	struct sockaddr_in serv_addr, cli_addr;
+	int queue = 10;
+	
+	// CLient
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
 	int n;
-	
+		
 public:
-	enum sock_role { server, client };
-	ezsock(sock_role role);				// Open
-	~ezsock(void);						// Close
-	string read_txt();					// Read
-	int read_bin(char* in);
-	string send_txt(string text);		// Write
-	void send_bin(char* out, int size);
+	ezsock(sock_role role, int port, const string& ip = string());	// Open
+	~ezsock(void);													// Close
+	string read_txt();												// Read
+	int read_bin(char *in);
+	void send_txt(const string &text);							// Write
+	void send_bin(char *out, int size);
 	
-	
+	void error(const string &msg);
 	void release(void);
 };
 //;)
